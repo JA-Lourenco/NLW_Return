c2bedef2883ca8793ed34d95df6e8b94a8b1f5ef
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-06 23:30:03 -0300

    Creating the Form component

--- MOBILE/src/components/Widget/index.tsx
+++ MOBILE/src/components/Widget/index.tsx
@@ -7,8 +7,11 @@ import { ChatTeardropDots } from 'phosphor-react-native'
 import BottomSheet from '@gorhom/bottom-sheet'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
+import { feedbackTypes } from '../../utils/feedbackTypes'
 import { theme } from '../../theme';
 import { styles } from './styles';
+
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null)
